aeda2f1da78667dc95afc6358e016a43c524f4d8
EC2 Default User <ec2-user@ip-172-31-40-246.us-east-2.compute.internal>
2021-03-22 09:01:49 +0000

    提出用

--- main.js
+++ main.js
@@ -2,47 +2,48 @@ function update( _v ) {
   document.querySelector( 'input' ).value = _v
 }
 
-	function append( _v ) {
+function append( _v ) {
 		document.querySelector( 'input' ).value += _v
       if ((document.querySelector( 'input' ).value == "00")){
 		    document.querySelector( 'input' ).value = "0";
-	      }else{
+	      }
 	    if ((document.querySelector( 'input' ).value == "000")){
 		    document.querySelector( 'input' ).value = "0";
-	      }else{
+	      }
 	    if ((document.querySelector( 'input' ).value == "01")){
 		    document.querySelector( 'input' ).value = "1";
-	      }else{
+	      }
 	    if ((document.querySelector( 'input' ).value == "02")){
 		    document.querySelector( 'input' ).value = "2";
-	      }else{
+	      }
 	    if ((document.querySelector( 'input' ).value == "03")){
 		    document.querySelector( 'input' ).value = "3";
-	     }else{
+	     }
 	    if ((document.querySelector( 'input' ).value == "04")){
 		    document.querySelector( 'input' ).value = "4";
-	     }else{
+	     }
 	    if ((document.querySelector( 'input' ).value == "05")){
 		    document.querySelector( 'input' ).value = "5";
-	     }else{
+	     }
 	    if ((document.querySelector( 'input' ).value == "06")){
 		    document.querySelector( 'input' ).value = "6";
-	     }else{
+	     }
 	    if ((document.querySelector( 'input' ).value == "07")){
 		    document.querySelector( 'input' ).value = "7";
-	     }else{
+	     }
 	    if ((document.querySelector( 'input' ).value == "08")){
 		    document.querySelector( 'input' ).value = "8";
-	     }else{
+	     }
 	    if ((document.querySelector( 'input' ).value == "09")){
 		    document.querySelector( 'input' ).value = "9";
-	     }else{
+	     }
 	    if ((document.querySelector( 'input' ).value == ".")){
-        document.querySelector( 'input' ).value = "";
+        document.querySelector( 'input' ).value = "0.";
+	    }
+      if (document.querySelector('input').value == "0..") {
+        document.querySelector( 'input' ).value = "0."
+      }
 	  }
-	  }
-	}}}}}}}}}}}
-
 
 function calc() {
   const v = document.querySelector( 'input' ).value
